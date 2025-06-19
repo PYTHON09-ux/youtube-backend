8ce1ab8b30201207539735b986ea4df8a662fd5d
PYTHON09-ux <itispavan24@gmail.com>
2025-06-19 17:28:44 +0530

    database connected successfully

--- src/index.js
+++ src/index.js
@@ -1,1 +1,42 @@
- 
+import connectDB from "./db/index.js";
+import donenv from 'dotenv'
+
+
+donenv.config({
+    path:'./env'
+})
+
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+/* import express from 'express'
+
+const app =express()
+
+;(async ()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error",(error)=>{
+            console.log("error in express")
+            throw error
+        })
+
+        app.listen(process.env.PORT,()=>{
+            console.log(`app is listening on port : ${process.env.PORT}`)
+        })
+    } catch (error) {
+        console.error("ERROR::", error)
+        throw error
+    }
+})()  // self executed block 
+
+*/
